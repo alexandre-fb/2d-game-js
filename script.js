bec70f5d99775b0a004d88ddeeecb8bca8ee6ed7
Alexandre Bisognin <alexandrefb@planmkt.com.br>
2024-08-31 14:27:27 -0300

    fix: canvas width

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ window.addEventListener("load", function () {
   // canvas setup
   const canvas = document.getElementById("canvas1");
   const ctx = canvas.getContext("2d");
-  canvas.width = 500;
+  canvas.width = 700;
   canvas.height = 500;
 
   class InputHandler {
